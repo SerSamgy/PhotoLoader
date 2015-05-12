ab2e9490430403bc2e231b00329efabb5832ae44
samgy <samgy-cool@yandex.ru>
2015-05-12 22:11:53 +0300

    - Deleted on equal file names ValidationError; - Added border to the photos table

--- PhotoLoader/loader/models.py
+++ PhotoLoader/loader/models.py
@@ -1,29 +1,8 @@
 import hashlib
-from django.core.exceptions import ValidationError
-from django.core.files.storage import FileSystemStorage
 from django.db import models
 
-class PhotoFilesStorage(FileSystemStorage):
-    """
-    Custom storage class. Has overridden method _save for avoiding duplicate files saving.
-    """
-    def get_available_name(self, name, max_length=None):
-        """
-        Save the name of uploaded file the same.
-        """
-        return name
-
-    def _save(self, name, content):
-        """
-        Check if file exists in storage. Avoid default save if it does.
-        """
-        if self.exists(name):
-            raise ValidationError("Uploaded file already exists!", code='invalid')
-
-        return super(PhotoFilesStorage, self)._save(name, content)
-
 class Photo(models.Model):
-    image = models.ImageField(upload_to='photos/', storage=PhotoFilesStorage())
+    image = models.ImageField(upload_to='photos/')
     name = models.CharField(max_length=256, default="New Photo Name")
     model_name = models.CharField(max_length=256, default="Camera Model Name")
     create_date = models.DateTimeField()
